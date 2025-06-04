a0c68d12ea9976803934cdc4cfa36611421dfa70
Juhkang <juhyunk0820@naver.com>
2025-06-04 21:13:26 +0900

    로그인 화면 이동 테스트 및 커스텀 버튼 기능 테스트

--- app/(tabs)/my/index.tsx
+++ app/(tabs)/my/index.tsx
@@ -1,9 +1,25 @@
-import { SafeAreaView, Text, View } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import { router } from "expo-router";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function MyScreen() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      <CustomButton
+        label="테스트"
+        onPress={() => router.push("/auth")}
+        variant="outlined"
+      />
       <Text>내 화면</Text>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 30,
+  },
+});
