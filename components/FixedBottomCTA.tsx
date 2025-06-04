a31d29f9d78c5300256041180ac6ab66d7826a10
Juhkang <juhyunk0820@naver.com>
2025-06-04 21:14:35 +0900

    공용 바텀 버튼 컴포넌트 컴포넌트화

---
+++ components/FixedBottomCTA.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import CustomButton from "./CustomButton";
+import { colors } from "@/constants";
+
+interface FixedBottomCTAProps {
+  label: string;
+  onPress: () => void;
+}
+
+function FixedBottomCTA({ label, onPress }: FixedBottomCTAProps) {
+  const insets = useSafeAreaInsets();
+  return (
+    <View
+      style={[styles.buttonContainer, { paddingBottom: insets.bottom || 12 }]}
+    >
+      <CustomButton label="회원가입" onPress={() => console.log("로그인")} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    position: "absolute",
+    bottom: 24,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderTopColor: colors.GRAY_300,
+    paddingTop: 12,
+    paddingHorizontal: 16,
+  },
+});
+
+export default FixedBottomCTA;
