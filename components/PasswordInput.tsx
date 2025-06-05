7d5609a8d26bef99a543e7580b681a20acb3d164
Juhkang <juhyunk0820@naver.com>
2025-06-05 20:19:45 +0900

    PasswordInput 컴포넌트 수정 - ref, autofocusing, keyboard 설정

--- components/PasswordInput.tsx
+++ components/PasswordInput.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import { Controller, useFormContext } from "react-hook-form";
 import InputField from "./InputField";
 import { colors } from "@/constants";
+import { TextInputProps } from "react-native";
 
-function PasswordInput() {
-  const { control } = useFormContext();
+interface passwordInputProps {
+  submitBehavior?: TextInputProps["submitBehavior"];
+}
+
+function PasswordInput({
+  submitBehavior = "blurAndSubmit",
+}: passwordInputProps) {
+  const { control, setFocus } = useFormContext();
   return (
     <Controller
       name="password"
@@ -16,14 +23,20 @@ function PasswordInput() {
           }
         },
       }}
-      render={({ field: { onChange, value } }) => (
+      render={({ field: { ref, onChange, value } }) => (
         <InputField
+          ref={ref}
           label="비밀번호"
           placeholder="비밀번호를 입력해주세요"
           secureTextEntry
+          inputMode="text"
+          textContentType="oneTimeCode"
+          submitBehavior={submitBehavior}
+          autoCapitalize="none"
           placeholderTextColor={colors.GRAY_500}
           value={value}
           onChangeText={onChange}
+          onSubmitEditing={() => setFocus("passwordConfirm")}
         />
       )}
     />
