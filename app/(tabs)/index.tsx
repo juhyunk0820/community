a4c898b53d69d349e59d03bb7c661775ccfdd7a0
Juhkang <juhyunk0820@naver.com>
2025-06-06 18:46:36 +0900

    글 작성 아이콘 버튼 구현 및 표시

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,23 @@
 import FeedList from "@/components/FeedList";
 import { colors } from "@/constants";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import useAuth from "@/hooks/queries/useAuth";
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { Pressable, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
+  const { auth } = useAuth();
   return (
     <SafeAreaView style={styels.container}>
       <FeedList />
+      {auth.id && (
+        <Pressable
+          style={styels.writeButton}
+          onPress={() => router.push("/post/write")}
+        >
+          <Ionicons name="pencil" size={32} color={colors.WHITE} />
+        </Pressable>
+      )}
     </SafeAreaView>
   );
 }
@@ -14,5 +26,21 @@ const styels = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.WHITE,
+  },
+  writeButton: {
+    position: "absolute",
+    bottom: 16,
+    right: 16,
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: colors.ORANGE_600,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: colors.BLACK,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    elevation: 2,
   },
 });
