936da7d2f452bf3d0c40690d3c6af3813fedf7f2
Juhkang <juhyunk0820@naver.com>
2025-06-06 18:47:52 +0900

    RootRouting Stack - post 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,9 @@ import { Stack } from "expo-router";
 import "react-native-reanimated";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "@/api/queryClient";
+import Toast, { ToastConfig } from "react-native-toast-message";
+import useAuth from "@/hooks/queries/useAuth";
+import { useEffect } from "react";
 
 export default function RootLayout() {
   const [loaded] = useFonts({
@@ -17,15 +20,29 @@ export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
       <RootNavigator />
+      <Toast />
     </QueryClientProvider>
   );
 }
 
 function RootNavigator() {
+  const { auth } = useAuth();
+
+  useEffect(() => {
+    auth.id &&
+      Toast.show({
+        type: "success",
+        text1: `환영합니다! ${auth.nickname || "회원"}님!`,
+        position: "top",
+        visibilityTime: 2500,
+      });
+  }, [auth.id]);
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="auth" options={{ headerShown: false }} />
+      <Stack.Screen name="post" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
   );
