f4ba06ea77472ca280951961914d37904b3505ff
Juhkang <juhyunk0820@naver.com>
2025-06-10 21:16:31 +0900

    headerRight: 저장 버튼 구현 및 기능 구현

--- app/post/write.tsx
+++ app/post/write.tsx
@@ -1,27 +1,55 @@
+import CustomButton from "@/components/CustomButton";
 import DescriptionInput from "@/components/DescriptionInput";
 import TitleInput from "@/components/TitleInput";
+import useCreatePost from "@/hooks/queries/useCreatePost";
+import { ImageUri } from "@/types";
+import { useNavigation } from "expo-router";
+import { useEffect } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { StyleSheet, View } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 type FormValues = {
   title: string;
   description: string;
+  imageUris: ImageUri[];
 };
 
 export default function WritePostScreen() {
+  const navigation = useNavigation();
+  const createPost = useCreatePost();
   const postForm = useForm<FormValues>({
     defaultValues: {
       title: "",
       description: "",
+      imageUris: [],
     },
   });
+
+  const onSubmit = (formValues: FormValues) => {
+    createPost.mutate(formValues);
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <CustomButton
+          label="작성"
+          size="small"
+          variant="standard"
+          onPress={postForm.handleSubmit(onSubmit)}
+        />
+      ),
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <FormProvider {...postForm}>
+    <FormProvider {...postForm}>
+      <KeyboardAwareScrollView contentContainerStyle={styles.container}>
         <TitleInput />
         <DescriptionInput />
-      </FormProvider>
-    </View>
+      </KeyboardAwareScrollView>
+    </FormProvider>
   );
 }
 
