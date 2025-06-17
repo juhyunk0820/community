fd3f3ef168538c406f420f1f92a15e54dd499794
Juhkang <juhyunk0820@naver.com>
2025-06-17 15:07:43 +0900

    함수 업데이트: getPosts

--- api/post.ts
+++ api/post.ts
@@ -1,4 +1,4 @@
-import { CreatePostDto } from "@/types";
+import { CreatePostDto, Post } from "@/types";
 import axiosInstance from "./axios";
 
 async function createPost(body: CreatePostDto) {
@@ -7,4 +7,10 @@ async function createPost(body: CreatePostDto) {
   return data;
 }
 
-export { createPost };
+async function getPosts(page = 1): Promise<Post[]> {
+  const { data } = await axiosInstance.get(`/posts?page=${page}`);
+
+  return data;
+}
+
+export { createPost, getPosts };
