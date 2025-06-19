ea02ea8f6cb80a9fcd662c177f9b3cd440ead780
Juhkang <juhyunk0820@naver.com>
2025-06-19 20:16:14 +0900

    게시물 업데이트 화면 구현

---
+++ app/post/update/[id].tsx
@@ -0,0 +1,74 @@
+import CustomButton from "@/components/CustomButton";
+import DescriptionInput from "@/components/DescriptionInput";
+import TitleInput from "@/components/TitleInput";
+import useCreatePost from "@/hooks/queries/useCreatePost";
+import useGetPost from "@/hooks/queries/useGetPost";
+import useUpdatePost from "@/hooks/queries/useUpdatePost";
+import { ImageUri } from "@/types";
+import { router, useLocalSearchParams, useNavigation } from "expo-router";
+import { useEffect } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { StyleSheet, View } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+type FormValues = {
+  title: string;
+  description: string;
+  imageUris: ImageUri[];
+};
+
+export default function PostUpdateScreen() {
+  const { id } = useLocalSearchParams();
+  const { data: post } = useGetPost(Number(id));
+  const updatePost = useUpdatePost();
+  const navigation = useNavigation();
+
+  const postForm = useForm<FormValues>({
+    defaultValues: {
+      title: post?.title,
+      description: post?.description,
+      imageUris: post?.imageUris,
+    },
+  });
+
+  const onSubmit = (formValues: FormValues) => {
+    updatePost.mutate(
+      {
+        id: Number(id),
+        body: formValues,
+      },
+      {
+        onSuccess: () => router.back(),
+      }
+    );
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <CustomButton
+          label="작성"
+          size="small"
+          variant="standard"
+          onPress={postForm.handleSubmit(onSubmit)}
+        />
+      ),
+    });
+  }, []);
+
+  return (
+    <FormProvider {...postForm}>
+      <KeyboardAwareScrollView contentContainerStyle={styles.container}>
+        <TitleInput />
+        <DescriptionInput />
+      </KeyboardAwareScrollView>
+    </FormProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 16,
+    gap: 16,
+  },
+});
