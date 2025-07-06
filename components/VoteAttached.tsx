acd4fe0de391fb5fc841f01a74645fd3eea7e329
Juhkang <juhyunk0820@naver.com>
2025-07-06 21:13:00 +0900

    투표 첨부 알림 컴포넌트: 글작성 및 수정 시 표시 필요

---
+++ components/VoteAttached.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useFormContext, useWatch } from "react-hook-form";
+import { Pressable, StyleSheet, View } from "react-native";
+import InputField from "./InputField";
+import { Ionicons } from "@expo/vector-icons";
+
+function VoteAttached() {
+  const { control, setValue, resetField } = useFormContext();
+  const [isVoteAttached] = useWatch({
+    control,
+    name: ["isVoteAttached"],
+  });
+
+  return (
+    <>
+      {isVoteAttached && (
+        <InputField
+          variant="outlined"
+          editable={false}
+          value="투표가 첨부되었습니다."
+          rightChild={
+            <Pressable
+              onPress={() => {
+                setValue("isVoteAttached", false);
+                resetField("voteOptions");
+              }}
+            >
+              <Ionicons name="close" size={20} color="black" />
+            </Pressable>
+          }
+        />
+      )}
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default VoteAttached;
