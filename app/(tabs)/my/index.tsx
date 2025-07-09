24b0ca39ee3e4951d65d7ebe83f37e0a3cb426be
Juhkang <juhyunk0820@naver.com>
2025-07-09 18:51:33 +0900

    마이페이지 구현: tabBar 구현중

--- app/(tabs)/my/index.tsx
+++ app/(tabs)/my/index.tsx
@@ -1,21 +1,108 @@
+import { baseUrls } from "@/api/axios";
 import AuthRoute from "@/components/AuthRoute";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import Tab from "@/components/Tab";
+import { colors } from "@/constants";
+import useAuth from "@/hooks/queries/useAuth";
+import { useState } from "react";
+import { Image, Platform, StyleSheet, Text, View } from "react-native";
 
 export default function MyScreen() {
+  const { auth } = useAuth();
+  const [currentTab, setCurrentTab] = useState<0 | 1>(0);
+
+  const handleTabPress = (tabIndex: 0 | 1) => {
+    setCurrentTab(tabIndex);
+  };
+
   return (
     <AuthRoute>
-      <SafeAreaView style={styles.container}>
-        <Text>내 화면</Text>
-      </SafeAreaView>
+      <View style={styles.headerContainer}>
+        <Image
+          style={styles.image}
+          source={
+            auth.imageUri
+              ? {
+                  uri: `${
+                    Platform.OS === "ios" ? baseUrls.ios : baseUrls.android
+                  }/${auth.imageUri}`,
+                }
+              : require("@/assets/images/default-avatar.png")
+          }
+        />
+        <CustomButton
+          size="medium"
+          variant="outlined"
+          label="프로필 편집"
+          style={{ position: "absolute", right: 16, bottom: 16 }}
+        />
+
+        {/* <View style={styles.profileEditButtonContainer}>
+          <CustomButton size="medium" variant="outlined" label="프로필 편집" />
+        </View> */}
+      </View>
+      <View style={styles.container}>
+        <View style={styles.profile}>
+          <Text style={styles.nickname}>{auth.nickname}</Text>
+          <Text style={styles.introduce}>{auth.introduce}</Text>
+        </View>
+      </View>
+
+      <View style={styles.tabContainer}>
+        <Tab isActive={currentTab === 0} onPress={() => setCurrentTab(0)}>
+          게시물
+        </Tab>
+        <Tab isActive={currentTab === 1} onPress={() => setCurrentTab(1)}>
+          게시물
+        </Tab>
+      </View>
     </AuthRoute>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  headerContainer: {
+    position: "relative",
+    backgroundColor: colors.ORANGE_200,
+    width: "100%",
+    height: 154,
+    flexDirection: "row",
+  },
+  image: {
+    position: "absolute",
+    top: 77,
+    left: 16,
+    width: 154,
+    height: 154,
+    borderRadius: 77,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: colors.GRAY_500,
+  },
+  profileEditButtonContainer: {
+    position: "absolute",
+    top: 50,
+    right: 16,
+    width: 154,
+    height: 154,
     justifyContent: "center",
     alignItems: "center",
-    marginHorizontal: 30,
+  },
+  container: {
+    marginTop: 77,
+  },
+  profile: {
+    padding: 16,
+    gap: 16,
+  },
+  nickname: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  introduce: {
+    fontSize: 14,
+    color: colors.GRAY_700,
+  },
+  tabContainer: {
+    flexDirection: "row",
   },
 });
