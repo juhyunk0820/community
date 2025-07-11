b03b595518af833e29936038176a44f965fd7653
Juhkang <juhyunk0820@naver.com>
2025-07-11 20:13:45 +0900

    api추가: 게시글 작성자 탐색 api

--- api/auth.ts
+++ api/auth.ts
@@ -31,4 +31,10 @@ async function getMe(): Promise<Profile> {
   return data;
 }
 
-export { postSignup, postLogin, getMe };
+async function getUserProfile(id: number): Promise<Profile> {
+  const { data } = await axiosInstance.get(`/auth/${id}`);
+
+  return data;
+}
+
+export { postSignup, postLogin, getMe, getUserProfile };
