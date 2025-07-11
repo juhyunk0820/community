eb813d679c4975f36ec162a90f70951bd73817b7
Juhkang <juhyunk0820@naver.com>
2025-07-11 21:23:56 +0900

    프로필 수정 화면 경로 추가

--- app/(tabs)/my/_layout.tsx
+++ app/(tabs)/my/_layout.tsx
@@ -1,10 +1,34 @@
-import { Stack } from "expo-router";
+import { colors } from "@/constants";
+import { Foundation } from "@expo/vector-icons";
+import { router, Stack } from "expo-router";
+import { Pressable } from "react-native";
 import "react-native-reanimated";
 
 export default function MyLayout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerTintColor: colors.BLACK,
+        contentStyle: {
+          backgroundColor: colors.WHITE,
+        },
+      }}
+    >
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="edit"
+        options={{
+          headerShown: true,
+          headerBackButtonDisplayMode: "minimal",
+          headerShadowVisible: false,
+          headerTitle: "프로필 편집",
+          headerLeft: () => (
+            <Pressable onPress={() => router.back()}>
+              <Foundation name="arrow-left" size={28} color={"black"} />
+            </Pressable>
+          ),
+        }}
+      />
     </Stack>
   );
 }
