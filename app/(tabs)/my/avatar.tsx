78886c0a90ff0b7bbc207e8fe37ba490cef89beb
Juhkang <juhyunk0820@naver.com>
2025-07-13 23:22:41 +0900

    avatar 미리보기 기능 구현

--- app/(tabs)/my/avatar.tsx
+++ app/(tabs)/my/avatar.tsx
@@ -6,9 +6,11 @@ import useAuth from "@/hooks/queries/useAuth";
 import useGetAvatarItems from "@/hooks/queries/useGetAvatarItems";
 import { useNavigation } from "expo-router";
 import { useEffect, useRef, useState } from "react";
-import { FlatList, StyleSheet, View } from "react-native";
+import { FlatList, Platform, StyleSheet, View } from "react-native";
 import PagerView from "react-native-pager-view";
 import Toast from "react-native-toast-message";
+import { SvgUri } from "react-native-svg";
+import { baseUrls } from "@/api/axios";
 
 export default function AvatarScreen() {
   const navigation = useNavigation();
@@ -22,7 +24,7 @@ export default function AvatarScreen() {
     topId: auth?.topId ?? "",
     bottomId: auth?.bottomId ?? "",
     handId: auth?.handId ?? "",
-    skinId: auth?.skinId ?? "",
+    skinId: auth?.skinId ?? "01",
   });
 
   const getImageId = (url: string) => {
@@ -50,6 +52,14 @@ export default function AvatarScreen() {
     });
   };
 
+  const getAvatarItemUrl = (category: string, id?: string) => {
+    const baseUrl = Platform.OS === "ios" ? baseUrls.ios : baseUrls.android;
+    if (category === "default" || !id) {
+      return `${baseUrl}/default/frame.svg`;
+    }
+    return `${baseUrl}/items/${category}/${id}.svg`;
+  };
+
   useEffect(() => {
     navigation.setOptions({
       headerStyle: {
@@ -61,6 +71,50 @@ export default function AvatarScreen() {
   return (
     <>
       <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <View style={styles.avatarContainer}>
+            {avatarItem.hatId && (
+              <SvgUri
+                uri={getAvatarItemUrl("hats", avatarItem.hatId)}
+                style={[styles.avatar, { zIndex: 70 }]}
+              />
+            )}
+            {avatarItem.faceId && (
+              <SvgUri
+                uri={getAvatarItemUrl("faces", avatarItem.faceId)}
+                style={[styles.avatar, { zIndex: 60 }]}
+              />
+            )}
+            {avatarItem.topId && (
+              <SvgUri
+                uri={getAvatarItemUrl("tops", avatarItem.topId)}
+                style={[styles.avatar, { zIndex: 50 }]}
+              />
+            )}
+            {avatarItem.bottomId && (
+              <SvgUri
+                uri={getAvatarItemUrl("bottoms", avatarItem.bottomId)}
+                style={[styles.avatar, { zIndex: 40 }]}
+              />
+            )}
+            <SvgUri
+              uri={getAvatarItemUrl("default")}
+              style={[styles.avatar, { zIndex: 30 }]}
+            />
+            {avatarItem.skinId && (
+              <SvgUri
+                uri={getAvatarItemUrl("skins", avatarItem.skinId)}
+                style={[styles.avatar, { zIndex: 20 }]}
+              />
+            )}
+            {avatarItem.handId && (
+              <SvgUri
+                uri={getAvatarItemUrl("hands", avatarItem.handId)}
+                style={[styles.avatar, { zIndex: 10 }]}
+              />
+            )}
+          </View>
+        </View>
         <View style={styles.tabContainer}>
           {["모자", "얼굴", "상의", "하의", "손", "피부색"].map(
             (label, index) => (
@@ -80,7 +134,7 @@ export default function AvatarScreen() {
           initialPage={0}
           onPageSelected={(e) => setCurrentTab(e.nativeEvent.position)}
         >
-          {[
+          {/* {[
             { data: hats, name: "hatId", id: avatarItem.hatId },
             { data: faces, name: "faceId", id: avatarItem.faceId },
             { data: tops, name: "topId", id: avatarItem.topId },
@@ -102,7 +156,85 @@ export default function AvatarScreen() {
                 />
               )}
             />
-          ))}
+          ))} */}
+          <FlatList
+            data={hats}
+            keyExtractor={(item, index) => String(index)}
+            numColumns={3}
+            contentContainerStyle={styles.listContainer}
+            renderItem={({ item }) => (
+              <AvatarItem
+                uri={item}
+                isSelected={getImageId(item) === avatarItem.hatId}
+                onPress={() => handlePressItem("hatId", item)}
+              />
+            )}
+          />
+          <FlatList
+            data={faces}
+            keyExtractor={(item, index) => String(index)}
+            numColumns={3}
+            contentContainerStyle={styles.listContainer}
+            renderItem={({ item }) => (
+              <AvatarItem
+                uri={item}
+                isSelected={getImageId(item) === avatarItem.faceId}
+                onPress={() => handlePressItem("faceId", item)}
+              />
+            )}
+          />
+          <FlatList
+            data={tops}
+            keyExtractor={(item, index) => String(index)}
+            numColumns={3}
+            contentContainerStyle={styles.listContainer}
+            renderItem={({ item }) => (
+              <AvatarItem
+                uri={item}
+                isSelected={getImageId(item) === avatarItem.topId}
+                onPress={() => handlePressItem("topId", item)}
+              />
+            )}
+          />
+          <FlatList
+            data={bottoms}
+            keyExtractor={(item, index) => String(index)}
+            numColumns={3}
+            contentContainerStyle={styles.listContainer}
+            renderItem={({ item }) => (
+              <AvatarItem
+                uri={item}
+                isSelected={getImageId(item) === avatarItem.bottomId}
+                onPress={() => handlePressItem("bottomId", item)}
+              />
+            )}
+          />
+          <FlatList
+            data={hands}
+            keyExtractor={(item, index) => String(index)}
+            numColumns={3}
+            contentContainerStyle={styles.listContainer}
+            renderItem={({ item }) => (
+              <AvatarItem
+                uri={item}
+                isSelected={getImageId(item) === avatarItem.handId}
+                onPress={() => handlePressItem("handId", item)}
+              />
+            )}
+          />
+          <FlatList
+            data={skins}
+            keyExtractor={(item, index) => String(index)}
+            numColumns={3}
+            contentContainerStyle={styles.listContainer}
+            renderItem={({ item }) => (
+              <AvatarItem
+                uri={item}
+                isSelected={getImageId(item) === avatarItem.skinId}
+                onPress={() => handlePressItem("skinId", item)}
+              />
+            )}
+          />
         </PagerView>
       </View>
 
@@ -114,6 +246,28 @@ export default function AvatarScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  headerContainer: {
+    alignItems: "center",
+    position: "relative",
+    backgroundColor: colors.ORANGE_200,
+    width: "100%",
+    height: 115,
+    marginBottom: 115,
+  },
+  avatarContainer: {
+    width: 229,
+    height: 229,
+    borderRadius: 229,
+    borderWidth: 1,
+    backgroundColor: colors.WHITE,
+    borderColor: colors.GRAY_200,
+    overflow: "hidden",
+  },
+  avatar: {
+    width: 229,
+    height: 229,
+    position: "absolute",
   },
   tabContainer: {
     flexDirection: "row",
